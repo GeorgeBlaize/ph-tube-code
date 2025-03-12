90a6efe5cbac02ad88dc4f77d74c4f83742797ae
Student ApnaCollege.in <92225843+GeorgeBlaize@users.noreply.github.com>
2025-03-12 15:54:31 +0600

    cards added

--- script/index.js
+++ script/index.js
@@ -8,6 +8,12 @@ function loadCategories() {
       //3 - send data to display
       .then((data) => displayCategories(data.categories));
   }
+
+  function loadVideos(){
+     fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+     .then(response=>response.json())
+     .then((data)=>displayVideos(data.videos));
+  }
   
 
   function displayCategories(categories) {
@@ -30,4 +36,48 @@ function loadCategories() {
     }
   }
 
+
+  const displayVideos=(videos)=>{
+    const videoContainer=document.getElementById("video-container");
+
+    videos.forEach((video)=>{
+        //element
+        console.log(video);
+
+        const videoCard=document.createElement("div");
+        videoCard.innerHTML=`
+         <div class="card bg-base-100">
+            <figure class="relative">
+              <img class="w-full h-[150px] object-cover"
+                src="${video.thumbnail}"
+                alt="Shoes" />
+                <span class="absolute bottom-2 right-2 text-sm text-white bg-black px-2 rounded">3hrs 56 min ago</span>
+            </figure>
+            <div class=" flex gap-3 px-0 py-5">
+            
+               <div class="profile">
+                <div class="avatar">
+                    <div class="ring-primary ring-offset-base-100 w-6 rounded-full ring ring-offset-2">
+                      <img src="${video.authors[0].profile_picture}" />
+                    </div>
+                  </div>
+               </div>
+               <div class="intro">
+                <h2 class="text-sm font-semibold">Midnight Serenade</h2>
+                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name}<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt=""></p>
+                <p class="text-sm text-gray-400">${video.others.views} views</p>
+               </div>
+              
+            </div>
+          </div>
+
+        
+        `
+        videoContainer.append(videoCard);
+    });
+      
+
+  };
+
 loadCategories(); // Call function to load categories
+loadVideos();
